5b89cd3c935cf14776e714f9fcf15242e0514778
Son Trinh <sontrinh179@gmail.com>
2017-11-06 14:11:37 +0700

    Update setting menu

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,74 +1,45 @@
 import * as React from "react";
 import { addNewQuestion } from "./redux/actionCreators";
 import { connect } from "react-redux";
-
+import Paper from "material-ui/Paper";
+import FontIcon from "material-ui/FontIcon";
 interface ISettings {
-  // addArea: (area: any) => string;
   selectedQuestionType: string;
   currentIndex: number;
   addNewQuestion: () => any;
-  // surveyData: any[];
 }
 
 const Settings: React.SFC<ISettings> = props => {
-  const {
-    currentIndex,
-    selectedQuestionType,
-    addNewQuestion,
-    // surveyData,
-  } = props;
-//   const areaQuestionTemplate = {
-//     type: "question",
-//     questionType: "",
-//     multipleAnswer: [],
-//     question: "",
-//   };
-//   const newQuestion: Types.IShortQuestion = {
-//     questionType: "shortQuestion",
-//     question: "",
-//     description: "",
-//     answers: [],
-//   };
+  const { currentIndex, selectedQuestionType, addNewQuestion } = props;
+
   return (
-    <div className="menu-settings">
-      <div>
-        <div className="settings-icon add-question" onClick={e => addNewQuestion()}>
-          <i className="fa fa-plus-circle" />
-        </div>
-      </div>
-      <div>
-        <div className="settings-icon add-text">
-          <i className="fa fa-font" aria-hidden="true" />
-        </div>
-      </div>
-      <div>
-        <div className="settings-icon">
-          <i className="fa fa-picture-o" aria-hidden="true" />
-        </div>
-      </div>
-      <div>
-        <div className="settings-icon">
-          <i className="fa fa-youtube-play" aria-hidden="true" />
-        </div>
-      </div>
-      <div>
-        <div className="settings-icon">
-          <i className="fa fa-bars" aria-hidden="true" />
-        </div>
-      </div>
-    </div>
+    <Paper className="menu-settings">
+      <Paper className="settings-icon" onClick={e => addNewQuestion()}>
+        <FontIcon className="material-icons">add_box</FontIcon>
+      </Paper>
+      <Paper className="settings-icon add-text">
+        <FontIcon className="material-icons">font_download</FontIcon>
+      </Paper>
+      <Paper className="settings-icon">
+        <FontIcon className="material-icons">image</FontIcon>
+      </Paper>
+      <Paper className="settings-icon">
+        <FontIcon className="material-icons">movie</FontIcon>
+      </Paper>
+      <Paper className="settings-icon">
+        <FontIcon className="material-icons">storage</FontIcon>
+      </Paper>
+    </Paper>
   );
 };
 
 const mapStateToProps = (state: any) => ({
-  // surveyData: state.surveyData,
   selectedQuestionType: state.stateStatus.selectedQuestionType,
-  currentIndex: state.stateStatus.currentIndex,
+  currentIndex: state.stateStatus.currentIndex
 });
 
 const mapDispatchToProps = (dispatch: any) => ({
-  // addArea: (area: any) => dispatch(addArea(area)),
-  addNewQuestion: () => dispatch(addNewQuestion()),
+  addNewQuestion: () => dispatch(addNewQuestion())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Settings);
